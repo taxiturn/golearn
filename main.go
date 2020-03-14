99fe567b47fbb5d12ca6a128c53534858facfca6
Vinodh Boopalan <vinodh.boopalan@contentdiv.com>
2020-03-13 23:45:09 -0500

    primitives

--- main.go
+++ main.go
@@ -2,7 +2,44 @@ package main
 
 import "fmt"
 
+//Declaration and Primitives
+//Pointers
+//Constants
+
+/*
+This
+is
+a
+multi-line
+comment
+*/
+
 func main() {
+	//Hello World!!
 	fmt.Println("Hello World!!")
 
+	demoPrimitives()
+
+}
+
+func demoPrimitives() {
+	//Declaration and Primitives
+	//---------------------------
+	var i int
+	i = 42
+	fmt.Println(i) //prints 42
+
+	var f float32 = 3.14 //concise declaration
+	fmt.Println(f)
+
+	firstName := "John"
+	fmt.Println(firstName) //implicit initialization
+
+	b := true
+	fmt.Println(b)
+
+	c := complex(3, 4)
+	fmt.Println(c)
+	r, im := real(c), imag(c)
+	fmt.Println(r, im)
 }
